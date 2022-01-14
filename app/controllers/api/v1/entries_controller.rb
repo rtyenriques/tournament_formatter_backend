9a8a7153bb435e22b2e18f6188252b69925d131e
= <rtyenriques@gmail.com>
2022-01-14 15:38:26 -0800

    added strong params for entry

--- app/controllers/api/v1/entries_controller.rb
+++ app/controllers/api/v1/entries_controller.rb
@@ -1,7 +1,20 @@
 class Api::V1::EntriesController < ApplicationController
 
     def index
-        @entries = Entry.all
-        render json: @entries
+        entries = Entry.all
+        render json: entries
     end
+
+    def create
+        entry = Entry.new(entry_params)
+
+
+    end
+
+    private
+
+    def entry_params
+      params.require(:entry).permit(:name, :location, :crew)
+    end
+
 end
