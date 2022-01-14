7c9210bf0b5906ca0040c027328975b7743fc2cd
= <rtyenriques@gmail.com>
2022-01-13 23:34:09 -0800

    competition model, migration and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+four = Competition.create(:name => '4 on 4 Breaking', :description => 'A 4 on 4 crew breaking battle being held at SkillzTalk.')
+one = Competition.create(:name => '1 on 1 Breaking', :description => 'A 1 on 1 solo breaking battle being held at SkillzTalk.')
+one_all = Competition.create(:name => '1 on 1 Kids Breaking', :description => 'A 1 on 1 kids breaking battle being held at SkillzTalk.')
