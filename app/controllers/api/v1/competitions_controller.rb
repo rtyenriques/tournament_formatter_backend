93a161e729dbe1d8611fe501f524c1a30ac37dae
= <rtyenriques@gmail.com>
2022-01-14 23:29:42 -0800

    added strong params and create for competition

--- app/controllers/api/v1/competitions_controller.rb
+++ app/controllers/api/v1/competitions_controller.rb
@@ -4,4 +4,19 @@ class Api::V1::CompetitionsController < ApplicationController
         competitions = Competition.all
         render json: CompetitionSerializer.new(competitions)
     end
+
+    def create
+        competition = Competition.new(competition_params)
+        if competition.save
+            render json: entry, status: :accepted
+        else
+            # render json: {errors: entry.full_messages}, status :unprocessible_entity
+        end
+     end
+
+     private
+
+     def competition_params
+        params.require(:competition).permit(:comp_type, :description)
+     end
 end
